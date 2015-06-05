55805d4c0d475174e832a42d5b4a5e1a1d358bbd
Venu Madhav Chitta <venu.chitta1992@gmail.com>
2015-06-04 17:57:01 -0700

    Initial Commit

---
+++ LocationStarter1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationStarter1
+//
+//  Created by S Venu Madhav Chitta on 6/4/15.
+//  Copyright (c) 2015 S Venu Madhav Chitta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LocationStarter1Tests/LocationStarter1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  LocationStarter1Tests.swift
+//  LocationStarter1Tests
+//
+//  Created by S Venu Madhav Chitta on 6/4/15.
+//  Copyright (c) 2015 S Venu Madhav Chitta. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LocationStarter1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
